df11338b7493148ba0ce8b495ec6e49803c94931
chen-yixin <ekikinn@foxmail.com>
2020-03-05 21:17:30 +0800

    5.4代码重构

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVistorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith听说有个很酷的在线代办事项应用
         # 她首先看了这个应用的主页
@@ -37,13 +42,7 @@ class NewVistorTest(unittest.TestCase):
         # 代办事项中显示了"1: Buy peacock feathers"
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            f"New to-do item did not appear in table. Contents were:\n{table.text}"
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # 页面中又出现了一个文本框,可以输入其他的待办事项
         # 她输入了"Use peacock feathers to make a fly" (使用孔雀羽毛做假蝇)
@@ -54,13 +53,8 @@ class NewVistorTest(unittest.TestCase):
         time.sleep(1)
 
         # 页面再次更新,她的清单中显示了这两个待办事项
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make a fly"', 
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Edith想知道这个网站是否会记住她的清单
         # 她看到网站为她生成了一个唯一的URL
