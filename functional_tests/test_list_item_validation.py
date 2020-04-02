ff8f5fd3f635a5566414adc3af7cc49ec4d4cea7
chen-yixin <ekikinn@foxmail.com>
2020-04-02 21:45:13 +0800

    功能测试:验证输入信息

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -8,15 +8,32 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         # Edith访问首页,不小心提交了一个空的待办事项
         # 输入框中没输入内容,她按下了回车键
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # 首页刷新了,显示一个错误消息
         # 提示代办事项不能是空
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        ))
 
         # 她输入了一些文字,然后再提交,这次没有问题了
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
 
         # 她有点儿调皮,又提交了一个空待办事项
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # 在清单页面她看到了一个类似的错误消息
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        ))
 
         # 输入文字后就没有问题了
-        self.fail('write me')
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for_row_in_list_table('2: Make tea')
